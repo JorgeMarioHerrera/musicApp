53424bd81ee4548f71ba4d07698d66c1a8e358d2
jorge mario herrera vargas <jorgemario2pac@gmail.com>
2021-01-04 15:57:53 -0500

    creacion de rutas y servicio de consumos de api

---
+++ musicApp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { useAnimation } from '@angular/animations';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

8b14a152d2caf58351545c1483cbfb1195c0fa84
jorge mario herrera vargas <jorgemario2pac@gmail.com>
2021-01-04 16:03:43 -0500

    Base proyecto

--- musicApp/src/app/components/home/home.component.ts
+++ musicApp/src/app/components/home/home.component.ts
@@ -7,14 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
-  paises: any[] = [];
 
-  constructor( private http: HttpClient) {
-    this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (data: any) => {
-      this.paises = data;
-      console.log(data);
-    });
-   }
+  constructor() {}
 
   ngOnInit(): void {
   }
